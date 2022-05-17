4eefe055c86cd5f0195d857999f425179496ff09
Gema Refantero <refara.08@gmail.com>
2022-05-17 10:04:44 +0700

    added navbar animation using gsap

--- src/component/Navigation/Navigation.js
+++ src/component/Navigation/Navigation.js
@@ -1,18 +1,51 @@
+import { useRef, useState } from "react";
+import gsap from "gsap";
 import * as ReactDOM from "react-dom";
 
 const Navigation = () => {
   const navContainer = document.getElementById("nav");
+  const hamburgerRef = useRef();
+  const q = gsap.utils.selector(hamburgerRef);
+
+  const [isHamburgerActive, setIsHamburgerActive] = useState(false);
+
+  const tl = gsap.timeline();
+
+  const toggleHamburger = () => {
+    setIsHamburgerActive((prev) => !prev);
+
+    if (!isHamburgerActive) {
+      tl.to(q(".line1"), { yPercent: 400 })
+        .to(q(".line2"), { opacity: 0 }, "<")
+        .to(q(".line3"), { yPercent: -400 }, "<")
+        .to(q(".line1"), { yPercent: 400, rotate: 45 }, "<.5")
+        .to(q(".line3"), { yPercent: -400, rotate: 135 }, "<");
+    } else {
+      tl.to(q(".line3"), { yPercent: -400, rotate: 0 })
+        .to(q(".line1"), { yPercent: 400, rotate: 0 }, "<")
+        .to(q(".line3"), { yPercent: 0 }, "<.5")
+        .to(q(".line2"), { opacity: 1 }, "<")
+        .to(q(".line1"), { yPercent: 0 }, "<");
+    }
+  };
 
   return (
     <>
       {ReactDOM.createPortal(
         <div className="flex flex-row justify-between items-center px-4 fixed top-0 left-0 w-full h-[70px] z-40">
-          <h3 className="text-3xl text-white font-bold">sunnyside</h3>
+          <h3 className="text-3xl text-white font-bold font-barlow">
+            sunnyside
+          </h3>
           <nav>
-            <div className="hamburger">
-              <span></span>
-              <span></span>
-              <span></span>
+            <div
+              ref={hamburgerRef}
+              // onClick={!isHamburgerActive ? openHamburger : closeHamburger}
+              onClick={toggleHamburger}
+              className="hamburger"
+            >
+              <span className="line1"></span>
+              <span className="line2"></span>
+              <span className="line3"></span>
             </div>
           </nav>
         </div>,
--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -1,12 +1,17 @@
 import iconArrowDownMobile from "../../img/icon-arrow-down.svg";
 
 const Header = (props) => {
+  const style = {
+    headerContent:
+      "container mx-auto h-[680px] flex flex-col justify-center items-center gap-12",
+    headerText:
+      "text-5xl font-bold text-center text-white font-fraunces uppercase tracking-widest",
+  };
+
   return (
-    <header className="container mx-auto">
-      <div className="header-content h-[680px] flex flex-col justify-center items-center gap-12">
-        <h1 className="text-5xl font-bold text-center text-white font-fraunces uppercase tracking-widest">
-          we are creatives
-        </h1>
+    <header className="header">
+      <div className={style.headerContent}>
+        <h1 className={style.headerText}>we are creatives</h1>
         <div className="">
           <img src={iconArrowDownMobile} alt="arrow down icon" />
         </div>
